67af6685e0bdd05a751348a2a0537651eba4791b
frankschweitzer <schwfj20@wfu.edu>
2024-12-08 21:09:38 -0500

    updating

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Hello world \n")
+	port := ":8080"
+	fmt.Printf("Starting server on port %s \n", port)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(port, nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "FIRST GO WEBSITE")
 }
